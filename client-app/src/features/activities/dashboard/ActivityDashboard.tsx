c8443748edff6934eb8e3f993b63395506abbeff
Janus McDonald <janusmcd101@gmail.com>
2024-03-26 14:14:10 +0200

    End of section 21

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,29 +1,55 @@
-import { Grid } from 'semantic-ui-react';
+import { Grid, Loader } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import ActivityFilters from './ActivityFilters';
+import { PagingParams } from '../../../app/models/pagination';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
-export default observer (function ActivityDashboard() {
+export default observer(function ActivityDashboard() {
 
-    const {activityStore} = useStore();
-    const {loadActivities, activityRegistry} = activityStore
+    const { activityStore } = useStore();
+    const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore
+    const [loadingNext, setLoadingNext] = useState(false);
+
+    function handleGetNext() {
+        setLoadingNext(true);
+        setPagingParams(new PagingParams(pagination!.currentPage + 1));
+        loadActivities().then(() => setLoadingNext(false));
+    }
 
     useEffect(() => {
-      if (activityRegistry.size <= 1) loadActivities();
+        if (activityRegistry.size <= 1) loadActivities();
     }, [loadActivities, activityRegistry.size])
-
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading Activities' />
 
     return (
         <Grid>
             <Grid.Column width='10'>
-               <ActivityList />
+                {activityStore.loadingInitial && activityRegistry.size ===0 && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder />
+                        <ActivityListItemPlaceholder />
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        dataLength={0}
+                        next={handleGetNext}
+                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                        loader={false}
+                    >
+                        <ActivityList />
+                    </InfiniteScroll>
+                )}
+
             </Grid.Column>
             <Grid.Column width='6'>
                 <ActivityFilters />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Loader active={loadingNext} />
             </Grid.Column>
         </Grid>
     )
--- client-app/src/features/activities/dashboard/ActivityFilters.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -1,17 +1,36 @@
+import { observer } from "mobx-react-lite";
 import Calendar from "react-calendar";
 import { Header, Menu } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
 
-export default function ActivityFilters() {
-    return (
+export default observer(function ActivityFilters() {
+    const {activityStore: {predicate, setPrediacate}} = useStore();
+
+    return (        
         <>
             <Menu vertical size='large' style={{ width: '100%', marginTop: 25 }}>
                 <Header icon='filter' attached color='teal' content='Filters' />
-                <Menu.Item content='All Activities' />
-                <Menu.Item content="I'm going" />
-                <Menu.Item content="I'm hosting" />
+                <Menu.Item 
+                    content='All Activities'
+                    active={predicate.has('all')}
+                    onClick={() => setPrediacate('all', 'true')}
+                />
+                <Menu.Item 
+                    content="I'm going" 
+                    active={predicate.has('isGoing')}
+                    onClick={() => setPrediacate('isGoing', 'true')}
+                />
+                <Menu.Item
+                    content="I'm hosting" 
+                    active={predicate.has('isHost')}
+                    onClick={() => setPrediacate('isHost', 'true')}
+                />
             </Menu>
             <Header />
-            <Calendar />
+            <Calendar 
+                onChange={(date) => setPrediacate('startDate', date as Date)}
+                value={predicate.get('startDate') || new Date}
+            />
         </>
     )
-}
+})
